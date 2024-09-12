f7b398fe7d372864540b9dec6cd0cd94040b7ac5
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-09-12 16:07:59 -0700

    fix baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/waveterm-docs-new/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

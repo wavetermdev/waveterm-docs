55cb8b659fae112d6dc37855a459c56620d42021
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-09-12 16:08:22 -0700

    fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   favicon: "img/wave-logo_appicon.svg",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://wavetermdev.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/waveterm-docs-new/",

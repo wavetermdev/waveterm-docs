9e851397e22de21693cf0be54026488d8f8673bb
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-09-12 15:49:40 -0700

    initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,80 @@
+import type {Config} from '@docusaurus/types';
+
+const config: Config = {
+  title: 'Wave Terminal Documentation',
+  tagline: 'Wavy Wavy Wavy',
+  favicon: 'img/wave-logo_appicon.svg',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'wavetermdev', // Usually your GitHub org/user name.
+  projectName: 'waveterm-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  plugins: [
+    ['content-docs', {
+      path: 'docs',
+      routeBasePath: '/'
+    } as import('@docusaurus/plugin-content-docs').Options],
+  ],
+  themes: [
+    'classic',
+  ],
+  themeConfig: {
+    docs: {
+      sidebar: {
+        hideable: false,
+        autoCollapseCategories: false,
+      },
+    },
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: true
+    },
+    navbar: {
+      logo: {
+        src: "img/wave-light.png",
+        srcDark: "img/wave-dark.png",
+        href: "https://waveterm.dev/",
+        height: "32px"
+      },
+      hideOnScroll: true,
+      items: [
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          label: "Github",
+          href: 'https://github.com/wavetermdev/waveterm',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ]
+    }
+  }
+};
+
+export default config;

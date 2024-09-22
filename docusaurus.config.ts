5e100c4cd28d4450dc73a9e868376cf71ffeeff8
sawka <mike@commandline.dev>
2024-09-22 13:11:24 -0700

    update betadocs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     favicon: "img/logo/wave-logo_appicon.svg",
 
     // Set the production url of your site here
-    url: "https://beta.docs.waveterm.dev/",
+    url: "https://betadocs.waveterm.dev/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",

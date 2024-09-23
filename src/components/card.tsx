20eb253883b2ee2f99f59c170e2b9454d165e196
Evan Simkowitz <esimkowitz@users.noreply.github.com>
2024-09-23 11:50:36 -0700

    more card work

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,19 @@
+import clsx from "clsx";
+import "./card.css";
+
 interface CardProps {
     icon: string;
     title: string;
     description: string;
+    href: string;
 }
 
-export default function Card({ icon, title, description }: CardProps) {
-    return <div className="card"></div>;
+export default function Card({ icon, title, description, href }: CardProps) {
+    return (
+        <a className="card" href={href}>
+            <div className={clsx("icon", "fa-sharp fa-regular", icon)} />
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </a>
+    );
 }
